88ce506eff74232a19f3adce7f11a7e5b603c85c
Uday Simha Reddy <udayreddy64@gmail.com>
2021-07-15 14:32:13 +0530

    Final Commit of the project...

---
+++ src/Success.java
@@ -0,0 +1,10 @@
+
+public class Success {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("All the projects successfully compiled");
+	}
+
+}
